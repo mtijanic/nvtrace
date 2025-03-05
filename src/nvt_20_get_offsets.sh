597496b2a6f500b2e6b194afd45494ef4aaea2bc
Milos Tijanic <mtijanic@nvidia.com>
2025-03-05 13:56:47 +0100

    Add offsets for 570.124.04 and 570.124.06

--- src/nvt_20_get_offsets.sh
+++ src/nvt_20_get_offsets.sh
@@ -37,6 +37,14 @@ case "$nvidia_version" in
 -DOFFSET_KernelGsp_pRpc=2344 \
 -DOFFSET_OBJRPC_rpcHistory=1192"
         ;;
+    "570.124.04"|"570.124.06")
+        offsets=" \
+-DOFFSET_OBJSYS_pGpuMgr=480 \
+-DOFFSET_OBJGPUMGR_gpuHandleIDList=257360 \
+-DOFFSET_OBJGPU_pKernelGsp=6216 \
+-DOFFSET_KernelGsp_pRpc=2368 \
+-DOFFSET_OBJRPC_rpcHistory=1272"
+        ;;
     *)
         echo "ERROR: Unknown NVIDIA driver version $nvidia_version"
         exit 1
